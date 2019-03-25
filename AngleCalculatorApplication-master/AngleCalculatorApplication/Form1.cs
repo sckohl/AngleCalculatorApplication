f2416d190b7e4f96a9674c36b15a08b3a2b178cd
sckohl <sckohl@gmail.com>
2019-03-25 09:37:48 -0700

    Add files via upload

---
+++ AngleCalculatorApplication-master/AngleCalculatorApplication/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AngleCalculatorApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_HelloWord_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(DataProcessing.Class1.message);
+
+        }
+
+        public void btn_Calculate_Click(object sender, EventArgs e)
+        {
+            DataProcessing.Class1 class1 = new DataProcessing.Class1(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            MessageBox.Show(class1.CalcResult);
+        }
+
+
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
---
+++ AngleCalculatorApplication-master/DataProcessing/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Forms;
+using System;
+
+
+
+namespace DataProcessing
+{
+    public class Class1
+    {
+        public Class1(int var1, int var2)
+        {
+            Var1 = var1;
+            Var2 = var2;
+            Calculate(Var1, Var2);
+        }
+
+        public int Var1 { get; set ; }
+        public int Var2 { get; set; }
+        public static string message = "Hello World";
+        public string CalcResult;
+                 
+        public string Calculate(int var1, int var2)
+        {
+            int result = var1 * var2;
+            
+            return CalcResult = Convert.ToString(result);
+        }
+
+    }
+
+}
